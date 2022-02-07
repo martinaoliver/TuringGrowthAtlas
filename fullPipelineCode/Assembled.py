82a4fb41a4daf6047461ffb7aafd4a7bfe81e773
smkyrle <smkyrle@github.com>
2022-02-07 15:58:47 +0000

    Multiprocessing added. Parameter and atlas storing changed to be dictionary indexed with tuple. Itertools product used to generate combinations of parameters and adjacency matrices.

--- fullPipelineCode/Assembled.py
+++ fullPipelineCode/Assembled.py
@@ -1,14 +1,56 @@
 from tqdm import tqdm
 import sys
+import multiprocessing as mp
+import traceback
+from itertools import product
+
 
 #########################################
 ################# SETUP #################
 #########################################
 
+def multiprocess_wrapper(function, items, cpu):
+
+    ###########################################
+    # Function: Parallelise input function    #
+    #                                         #
+    # Inputs: Function to parallelise         #
+    # (def score),                            #
+    # list of tuples as function input,       #
+    # number of threads to parallelise across #
+    #                                         #
+    # Output: List of returned results        #
+    ###########################################
+
+    processes = min(cpu, mp.cpu_count())
+
+    with mp.Pool(processes) as p:
+        results = dict(p.imap(function, items))
+        x = p.imap(function, items)
+        p.close()
+        p.join()
+    return results
+
+from Part3_SOLVE_v2 import Solver
+def run_solver(items):
+    items, args = items
+    try:
+        concs_list = []
+        for count, item in enumerate(items):
+            conc, ss = Solver.solve(p = item[0], topology = item[1], args = args)
+
+
+
+    except:
+        traceback.print_exc()
+        raise
+
+
+
 # Function for parsing command line arguments
 
 def parse_args(inputs):
-    
+
     args = dict(
         num_nodes=2,
         num_diffusers=2,
@@ -36,29 +78,30 @@ def parse_args(inputs):
 
 if __name__ == '__main__':
 
+    print("Parsing inputs...")
     args = parse_args(sys.argv)
 
     ################### PART ONE: ATLAS ########################
-    
+    print("Building atlas...")
     from Part1_ATLAS import Atlas_Builder
     atlas = Atlas_Builder()
     atlas = atlas.create_adjacency_matrices(nodes = args['num_nodes'], diffusers = args['num_diffusers'])
-    
+
     ################### PART TWO: PARAMETERS ###################
-     
-    from Part2_PARAMETERS import LHS  
-    nsamples = args['num_samples']   
+    print("Sampling parameters...")
+    from Part2_PARAMETERS import LHS
+    nsamples = args['num_samples']
     sampler = LHS(nsamples = args['num_samples'])
     params = sampler.sample_parameters()
-    
+
+    # Join altas and params
+    indexes = product(params.keys(), atlas.keys())
+    combinations = product(params.values(), atlas.values())
+
+    params_and_arrays = {index: combination for index,combination in zip(indexes, combinations)}
+
+    items = [(pa, args) for pa in params_and_arrays]
+
     ################### PART THREE: SOLVE ######################
-    
-    from Part3_SOLVE_v2 import Solver        
-    for s in tqdm(range(nsamples)):   
-        parameter_set = params.loc[s].to_dict() # Extract parameter set from dataframe.      
-        for R in atlas:   
-            conc = Solver.solve(p = parameter_set, topology = R, args = args)
-            #Solver.plot_conc(conc)
-        
-        
-        
+    print("Running solver...")
+    results = multiprocess_wrapper(run_solver, items, 2)
--- fullPipelineCode/Part2_PARAMETERS.py
+++ fullPipelineCode/Part2_PARAMETERS.py
@@ -16,7 +16,7 @@ class LHS: # Defines parameter sampling
             'k_xy'          :  [0.1, 100],
             'k_yy'          :  [0.1, 100],
             'k_yx'          :  [0.1, 100],
-            'diffusion_x'   :  [1],           # Diffusion constants.
+            'diffusion_x'   :  [1.],           # Diffusion constants.
             'diffusion_y'   :  [0.001, 1000],
             'n'             :  [2,4]          # Hill coefficient.
             }
@@ -35,7 +35,7 @@ class LHS: # Defines parameter sampling
 
     # Function generates distribution range for each parameter and performs latin hypercube sampling.
     def sample_parameters(self):
-        
+
         # def loguniform(low=-3, high=3, size=None):
         #     return (10) ** (np.random.uniform(low, high, size))
 
@@ -56,12 +56,13 @@ class LHS: # Defines parameter sampling
                 # Perform lhs using the resulting distribution.
                 samples = self.lhs(distribution)
 
-                param_sample_dict[p] = samples
-                
+                param_sample_dict[p] = samples.tolist()
+
             elif len(self.params[p]) == 1: # If one number in parameter range it is constant.
 
-                param_sample_dict[p] = self.params[p][0]
+                param_sample_dict[p] = [self.params[p][0] for i in range(self.nsamples)]
 
-        samples = pd.DataFrame(param_sample_dict)
-        
+        # Convert dictionary of lists into dictionary of dictionaries
+        samples = {count: dict(zip(param_sample_dict, i)) for count, i in enumerate(zip(*param_sample_dict.values()))}
+
         return samples
