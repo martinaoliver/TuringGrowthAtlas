bec0e176a5237345689fe6ac827d6aa5a0f8a290
jmj21-ic <98408217+jmj21-ic@users.noreply.github.com>
2022-02-21 09:27:33 +0000

    Add files via upload

---
+++ FixedParameters/Assembled.py
@@ -0,0 +1,129 @@
+from tqdm import tqdm
+import sys
+import multiprocessing as mp
+import traceback
+from itertools import product, chain
+
+
+
+#########################################
+################# SETUP #################
+#########################################
+
+def multiprocess_wrapper(function, items, cpu):
+
+    ###########################################
+    # Function: Parallelise input function    #
+    #                                         #
+    # Inputs: Function to parallelise         #
+    # (def score),                            #
+    # list of tuples as function input,       #
+    # number of threads to parallelise across #
+    #                                         #
+    # Output: List of returned results        #
+    ###########################################
+
+    processes = min(cpu, mp.cpu_count())
+
+    with mp.Pool(processes) as p:
+        results = list(tqdm(p.imap(function, items), total=len(items)))
+        x = p.imap(function, items)
+        p.close()
+        p.join()
+    return results
+
+from solver import Solver
+def run_solver(items):
+    index, settings, args = items
+    index_list = [i for i in index]
+
+    try:
+        concs, steadystates = Solver.solve(params = settings[0], topology = settings[1],  **args)
+        indexes = []
+        for i in range(len(concs)):
+            new_index = index_list + [i]
+            indexes.append(tuple(new_index))
+        concs = list(zip(indexes, concs))
+
+        steadystates = list(zip(indexes, steadystates))
+        return concs, steadystates
+
+    except:
+        traceback.print_exc()
+        raise
+
+
+
+# Function for parsing command line arguments
+
+def parse_args(inputs):
+
+    args = dict(
+        num_nodes=2,
+        num_diffusers=2,
+        system_length=200,
+        total_time=199,
+        num_samples=100,
+        growth="linear"
+    )
+
+    for a in inputs:
+
+        if "-" in a:
+            try:
+                command_line_input = int(inputs[inputs.index(a) + 1])
+            except:
+                command_line_input = inputs[inputs.index(a) + 1]
+            args[a[1:]] = command_line_input
+
+    return args
+
+
+#############################################
+################# EXECUTION #################
+#############################################
+
+if __name__ == '__main__':
+
+    print("Parsing inputs...")
+    args = parse_args(sys.argv)
+
+    ################### PART ONE: ATLAS ########################
+    print("Building atlas...")
+    from atlas import Atlas
+    atlas = Atlas()
+    atlas = atlas.create_adjacency_matrices(nodes = args['num_nodes'], diffusers = args['num_diffusers'])
+
+    ################### PART TWO: PARAMETERS ###################
+    print("Sampling parameters...")
+    from parameters_fixed import LHS
+    nsamples = args['num_samples']
+    sampler = LHS(nsamples = args['num_samples'])
+    params = sampler.sample_parameters()
+
+    # Prepare grid space, rate, and time
+    args["J"] = args["system_length"]
+    args["dx"] = args["J"] / (args["J"] - 1.)
+    args["num_timepoints"] = int(10. * args["total_time"])
+    args["dt"] = args["total_time"] / (args["num_timepoints"] - 1.)
+
+    for p in params:
+        # Calculate alpha values for each species.
+        for diff in ['diffusion_x', 'diffusion_y']:
+            params[p][f"alphan_{diff[-1]}"] = Solver.calculate_alpha(params[p][diff], **args)
+
+
+    # Join altas and params
+    indexes = product(params.keys(), atlas.keys())
+    combinations = product(params.values(), atlas.values())
+
+    params_and_arrays = {index: combination for index,combination in zip(indexes, combinations)}
+
+    items = [(pa, params_and_arrays[pa], args) for pa in params_and_arrays]
+
+
+    ################### PART THREE: SOLVE ######################
+    print("Running solver...")
+    results = multiprocess_wrapper(run_solver, items, 4)
+    results = {key:value for key, value in chain(*results)}
+    print(results)
---
+++ FixedParameters/parameters_fixed.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+
+class LHS: # Defines parameter sampling
+
+    def __init__(self, nsamples = 200, params = None):
+        self.nsamples = nsamples
+        self.params = {
+            'production_x'  :  [0.1],#[0.1, 100],    # Production rate.
+            'production_y'  :  [0.1],#[0.1, 100],
+            'degradation_x' :  [0.01],#[0.01,1],      # Degradation rate.
+            'degradation_y' :  [0.1],#[0.01,1],
+            'max_conc_x'    :  [3.1623],#[0.1, 100],    # Saturation.
+            'max_conc_y'    :  [100],#[0.1, 100],
+            'k_xx'          :  [100],#[0.1, 100],    # Reaction rates.
+            'k_xy'          :  [3.1623],#[0.1, 100],
+            'k_yy'          :  [100],#[0.1, 100],
+            'k_yx'          :  [3.1623],#[0.1, 100],
+            'diffusion_x'   :  [1.],           # Diffusion constants.
+            'diffusion_y'   :  [1000],#[0.001, 1000],
+            'n_xx'          :  [2,4],          # Hill coefficients.
+            'n_xy'          :  [2,4],
+            'n_yy'          :  [2,4],
+            'n_yx'          :  [2,4], 
+            }
+
+    # Function generates value from log distribution (0.001 to 1000).
+    def loguniform(self,low=-3, high=3, size=None):
+        return (10) ** (np.random.uniform(low, high, size))
+
+    # Function generates non-overlapping samples from a distribution (latin hypercube sampling).
+    # (data = distribution, nsample = number of desired samples)
+
+    def lhs(self,data):
+         P = 100*(np.random.permutation(self.nsamples) + 1 - np.random.uniform(size=(self.nsamples)))/self.nsamples
+         s = np.percentile(data, P)
+         return s
+
+    # Function generates distribution range for each parameter and performs latin hypercube sampling.
+    def sample_parameters(self):
+
+        # def loguniform(low=-3, high=3, size=None):
+        #     return (10) ** (np.random.uniform(low, high, size))
+
+        param_sample_dict = {}
+
+        for p in self.params:
+
+            if len(self.params[p]) == 2: # If 2 numbers in parameter range we do sampling.
+
+                distribution = []
+
+                # Sample from log distribution until reaching 1000 points within the specified range.
+                while len(distribution) < 1000:
+                    point = self.loguniform()
+                    if self.params[p][0] <= point <= self.params[p][1]:
+                        distribution.append(point)
+
+                # Perform lhs using the resulting distribution.
+                samples = self.lhs(distribution)
+
+                param_sample_dict[p] = samples.tolist()
+
+            elif len(self.params[p]) == 1: # If one number in parameter range it is constant.
+
+                param_sample_dict[p] = [self.params[p][0] for i in range(self.nsamples)]
+
+        # Convert dictionary of lists into dictionary of dictionaries
+        samples = {count: dict(zip(param_sample_dict, i)) for count, i in enumerate(zip(*param_sample_dict.values()))}
+
+        return samples
