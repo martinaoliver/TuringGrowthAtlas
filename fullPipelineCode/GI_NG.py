8dff2a3970723d35a43d6248e1c220b1452f53fd
jmj21-ic <98408217+jmj21-ic@users.noreply.github.com>
2022-03-22 14:54:05 +0000

    Update GI_NG.py

--- fullPipelineCode/GI_NG.py
+++ fullPipelineCode/GI_NG.py
@@ -1,28 +1,35 @@
 import pickle
 
-gi = [(1471, 15, 0),
-     (1471, 16, 0),
-     (2045, 15, 0),
-     (5177, 4, 0),
-     (5177, 16, 0),
-     (6218, 15, 0),
-     (6218, 16, 0),
-     (13964, 17, 0),
-     (31782, 16, 0),
-     (38381, 18, 0),
-     (38381, 20, 0),
-     (40812, 15, 0),
-     (46221, 18, 0)]
+gi = [(1471, 15),
+     (1471, 16),
+     (2045, 15),
+     (5177, 4),
+     (5177, 16),
+     (6218, 15),
+     (6218, 16),
+     (13964, 17),
+     (31782, 16),
+     (38381, 18),
+     (38381, 20),
+     (40812, 15),
+     (46221, 18)]
 
 print('Loading data...')
 
-with open('name.pkl', 'rb') as f:
+with open('2022-03-10_14-19None_results.pkl', 'rb') as f:
     data = pickle.load(f)
+      
+print('Checking for growth-induced patterns...')    
     
 ng_gi = {}
 
-for i in gi:
-    ng_gi[i] = data[i]
+for i in data:
+    if (i[0], i[1]) in gi:
+        ng_gi[i] = data[i]
     
+print('Results: ', ng_gi)
+
+print('Saving...')
+
 with open('Growth-induced_NonGrowth.pkl', 'wb') as f:
     pickle.dump(ng_gi, f)
